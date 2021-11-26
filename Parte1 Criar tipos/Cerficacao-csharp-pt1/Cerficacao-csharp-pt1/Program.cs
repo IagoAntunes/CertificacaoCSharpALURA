b8dc7cffe4626def1b3d6f7336ab2d7af6c9df25
iagoa <iagoantunes.f@gmail.com>
2021-11-26 14:43:16 -0300

    Adicionar arquivos de projeto.

---
+++ Parte1 Criar tipos/Cerficacao-csharp-pt1/Cerficacao-csharp-pt1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerficacao_csharp_pt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
