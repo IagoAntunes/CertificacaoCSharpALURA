4f463cf87e2c3a7955b45bebe1d60a9bd6395cb7
iagoa <iagoantunes.f@gmail.com>
2021-11-30 15:03:25 -0300

    Iago Antunes Ferreira

--- 3.EncapsulamentoeHirerarquia/3.EncapsulamentoeHirerarquia/Program.cs
+++ 3.EncapsulamentoeHirerarquia/3.EncapsulamentoeHirerarquia/Program.cs
@@ -10,6 +10,42 @@ namespace _3.EncapsulamentoeHirerarquia
     {
         static void Main(string[] args)
         {
+            Funcionario funcionario = new Funcionario();
+            /*
+            funcionario.salario = 1000;
+
+            Console.WriteLine(funcionario.salario);
+
+            funcionario.salario = -1200;
+            Console.WriteLine(funcionario.salario);
+            */
+            funcionario.Salario = 1200;
+            Console.WriteLine(funcionario.Salario);
+
+
+            Console.ReadLine();
         }
+    }
+    class Funcionario
+    {
+        private decimal salario;
+
+        public decimal Salario//Encapsulamento do campo Salario 
+        {
+            get
+            {
+                return salario;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Salario nao pode ser negativo!");
+                }
+                this.salario = value;
+            }
+        }
+
+
     }
 }
