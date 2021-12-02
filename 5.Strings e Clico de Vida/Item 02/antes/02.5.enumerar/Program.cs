106be2165e50ab68c04070463a63dfd7e4dec69d
iagoa <iagoantunes.f@gmail.com>
2021-12-02 14:48:23 -0300

    Iago Antunes Ferreira

--- 5.Strings e Clico de Vida/Item 02/antes/02.5.enumerar/Program.cs
+++ 5.Strings e Clico de Vida/Item 02/antes/02.5.enumerar/Program.cs
@@ -9,7 +9,23 @@ namespace _02._5.enumerar
             string documento = GetDocumento();
 
             Console.WriteLine(documento);
-            Console.WriteLine();
+
+            //Char c = documento[0];
+            //Char[] Array = documento.ToCharArray();
+
+            for (int i = 0; i < documento.Length; i++)
+            {
+                char c = documento[i];
+                if (char.IsUpper(c))
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ResetColor();
+                }
+                Console.Write(c);
+            }
 
             //TAREFA:
             //======
@@ -25,3 +41,19 @@ namespace _02._5.enumerar
         }
     }
 }
+/*05
+ * Pesquisando Strings
+ * Pesquisa de strings parte1
+ * -Descobrindo se uma strign conttem uma substring com o metodo Contains()
+ * -Desobrindo se uma strign comereça com uma substring com o metodo StartsWith()
+ * -Descobrindo se umma string termina com uma substring cm o metodo EndsWith()
+ *Pesquisa de stringa parte2
+ *-Obtendo o indice da peoxima substring dentor de uma string com o metodo IndexOf()
+ *-Obtendo uam substring a partir de uma string com o metodo SUbstring()
+ *Substituindo ocorrencias de uma substring em um texto com o metodo Replace()
+ *Enumerar strings
+ *-Varrengo strings com a instrução foreach
+ *-Varrendo strings com a instrução for
+ *Uma string pode ser iterada pela instrução foreach porque implementa a interface IEnumerable
+ *Descobrindo se um caractere é letra maiuscula com o metodo IsUpper()
+ */
