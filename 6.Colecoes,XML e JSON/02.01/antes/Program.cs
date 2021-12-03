dd4d59ff4e9fac68ac9fa34b189982e3e9ec19ce
iagoa <iagoantunes.f@gmail.com>
2021-12-03 15:51:34 -0300

    Iago Antunes Ferreira

--- 6.Colecoes,XML e JSON/02.01/antes/Program.cs
+++ 6.Colecoes,XML e JSON/02.01/antes/Program.cs
@@ -6,7 +6,30 @@ namespace _02._01
     {
         static void Main(string[] args)
         {
+            string alura = "Alura";
+            string caelum = "Caelum";
+            string casaDoCodigo = "Casa do Codigo";
 
+            Console.WriteLine(alura);
+            Console.WriteLine(caelum);
+            Console.WriteLine(casaDoCodigo);
+
+            //Matriz
+            //string[] empresas = new string[3];
+            ////empresas[0] = alura;
+            ////empresas[1] = caelum;
+            ////empresas[2] = casaDoCodigo;
+
+            //string[] empresas = new string[]
+            //{
+            //    alura,
+            //    caelum,
+            //    casaDoCodigo
+            //};
+
+            string[] empresas = { alura,caelum,casaDoCodigo};
+
+            Console.ReadLine();
         }
     }
 }
