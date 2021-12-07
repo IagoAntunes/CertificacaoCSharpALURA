fc78f7417ed78bf4d8fb7b469d5f0114352d2ed5
iagoa <iagoantunes.f@gmail.com>
2021-12-07 08:30:19 -0300

    Iago Antunes Ferreira

--- 6.Colecoes,XML e JSON/02.05/antes/Program.cs
+++ 6.Colecoes,XML e JSON/02.05/antes/Program.cs
@@ -13,7 +13,55 @@ namespace _02._05
             //TAREFA: Implementar uma fila de pedágio
 
             ///<image url="$(ProjectDir)\Slides\queue0.png" scale=""/>
+            var pedagio = new Pedagio();
+            pedagio.Enfileirar("Van");
+            pedagio.Enfileirar("Kombi");
+            pedagio.Enfileirar("Guincho");
+            pedagio.Enfileirar("Pickup");
 
+            pedagio.Desenfileirar();
+            pedagio.Desenfileirar();
+
+
+            Console.ReadKey();
+        }
+    }
+    class Pedagio
+    {
+        Queue<string> fila = new Queue<string> ();
+
+        public void Enfileirar(string veiculo)
+        {
+            fila.Enqueue(veiculo);
+            Console.WriteLine("Entrou na fila..." + veiculo);
+            Console.WriteLine();
+            Console.WriteLine("VEICULOS NA FILA");
+            Imprimir();
+        }
+
+        private void Imprimir()
+        {
+            foreach (var v in fila)
+            {
+                Console.WriteLine(v);
+            }
+            Console.WriteLine();
+        }
+
+        public void Desenfileirar()
+        {
+            if(fila.Count == 0)
+            {
+                Console.WriteLine("Fila Vazia!!");
+                return;
+            }
+            Console.WriteLine("Peek:"+fila.Peek());
+            var veiculo = fila.Dequeue();
+            Console.WriteLine("Saiu da fila..."+veiculo);
+            Imprimir();
+            string proximo;
+            fila.TryPeek(out proximo);
+            Console.WriteLine("O proximo da fila é:" + proximo);
         }
     }
 }
