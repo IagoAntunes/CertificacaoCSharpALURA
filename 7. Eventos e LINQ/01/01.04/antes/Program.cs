bbb12ec7c3e77dc3b69a8b9e21d0995ac584e2aa
iagoa <iagoantunes.f@gmail.com>
2021-12-07 14:26:11 -0300

    Iago Antunes Ferreira

--- 7. Eventos e LINQ/01/01.04/antes/Program.cs
+++ 7. Eventos e LINQ/01/01.04/antes/Program.cs
@@ -4,8 +4,27 @@ namespace _01_04
 {
     class Program
     {
+        delegate int Operacao(int a,int b);
         static void Main(string[] args)
         {
+
+            //Operacao operacao = (x, y) => { return x + y; };
+            Operacao operacao = (x, y) =>  x + y;
+            Console.WriteLine(operacao(3,2));
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
+/*03
+ * Delegados e Lambda
+ * -Criando delegados como ponteiros para metodos
+ * expressao lambda e metodos anonimos
+ * Utiliza lambdas e economize a criação de metodos
+ * utilize metodos anonimos e economize a criação de classes
+ */
