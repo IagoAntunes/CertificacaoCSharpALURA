2cfba4aa9718b7439cc1cd4538fb861fe08e786b
iagoa <iagoantunes.f@gmail.com>
2021-12-07 15:00:35 -0300

    Iago Antunes Ferreira

--- 7. Eventos e LINQ/01/01.04/antes/Program.cs
+++ 7. Eventos e LINQ/01/01.04/antes/Program.cs
@@ -15,9 +15,19 @@ namespace _01_04
             Func<int, int, int> somar = (x, y) => x + y;
             Console.WriteLine(somar(3,2));
 
-            Action<string> imprimirMensagem = (mensagem) => { Console.WriteLine(mensagem); };
 
+            Action<string> imprimirMensagem = (mensagem) => { Console.WriteLine(mensagem); };
             imprimirMensagem("Ola mundo");
+
+
+            var numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("Numeros divisiveis por 3:");
+            Predicate<int> divisivelPor3 = (numero) => numero % 3 == 0;
+            var divisiveis = Array.FindAll(numeros, divisivelPor3);
+            foreach (var numero in divisiveis)
+            {
+                Console.WriteLine(numero);
+            }
 
 
             Console.ReadLine();
@@ -30,4 +40,13 @@ namespace _01_04
  * expressao lambda e metodos anonimos
  * Utiliza lambdas e economize a criação de metodos
  * utilize metodos anonimos e economize a criação de classes
+ */
+/*04
+ * Func,Action,Predicate
+ * -Delegado Func e Delegado Action
+ * -O delegado de funcao Func
+ * -O delegado de funcao booleana predicate
+ * -O delegado de metodo sem retorno Action
+ * -Criando e consumindo predicados
+ * -Usando predicados em parametros de metodos do framework
  */
