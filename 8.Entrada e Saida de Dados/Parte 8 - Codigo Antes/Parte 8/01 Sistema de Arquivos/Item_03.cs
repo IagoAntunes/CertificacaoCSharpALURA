4be6bf70fce215b9c856aa9b1044868fae2dd5c7
iagoa <iagoantunes.f@gmail.com>
2021-12-08 14:10:14 -0300

    Iago Antunes Ferreira

--- 8.Entrada e Saida de Dados/Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_03.cs
+++ 8.Entrada e Saida de Dados/Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_03.cs
@@ -7,10 +7,23 @@ namespace Listings
 {
     class Item_03 //StreamWriter and StreamReader
     {
-        static void XMain(string[] args)
+        static void Main(string[] args)
         {
             //TAREFA: Usar FileStream dá muito trabalho...
             //        Não podemos usar algo mais simples??
+
+            
+            using(StreamWriter gravadorFluxo = new StreamWriter("ArquivoSaida.txt"))
+            {
+                gravadorFluxo.Write("Olá, Alura! (gravado com StreamWriter)");
+            }
+            using(StreamReader leitorFluxo = new StreamReader("ArquivoSaida.txt"))
+            {
+                string texto = leitorFluxo.ReadToEnd();
+                Console.WriteLine(texto);
+            }
+
+
 
         }
     }
