6ddd9903758a4345e632876c42d1588f646b1f99
iagoa <iagoantunes.f@gmail.com>
2021-12-08 16:49:05 -0300

    Iago Antunes Ferreira

--- 8.Entrada e Saida de Dados/Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_12.cs
+++ 8.Entrada e Saida de Dados/Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_12.cs
@@ -7,12 +7,43 @@ namespace Listings
 {
     class Item_12 //Procurando e listando arquivos
     {
-        static void XMain(string[] args)
+        static void Main(string[] args)
         {
             //TAREFAS:
             //Obter o diretório de início do projeto
             //Listar todos os diretórios do projeto
             //Listar todos os arquivos csharp (.cs) do projeto
+
+
+            DirectoryInfo diretorioInicial = new DirectoryInfo(@"..\..\..");
+            diretorioInicial.GetDirectories();
+            ListarDiretorios(diretorioInicial);
+
+        }
+
+        private static void ListarDiretorios(DirectoryInfo diretorioInicial)
+        {
+            foreach (var diretorio in diretorioInicial.GetDirectories())
+            {
+                Console.WriteLine(diretorio.FullName);
+
+                foreach (var arquivo in diretorio.GetFiles("*.cs"))
+                {
+                    Console.WriteLine(arquivo.FullName);
+                }
+
+                ListarDiretorios(diretorio);
+            }
         }
     }
 }
+/*04 Gerenciando Diretorios
+ * Manipular diretorios com a classe Directory
+ * CreateDirectory e Delete
+ * Obter informações de diretório
+ * A classe DirectoryInfo
+ * Diretorios de pastas espedciais do sistema
+ * Combinar diretorios com Path.Combine
+ * Pesquisar arquivos em diretorio com Directory.GetFiles
+ * Função Recursiva em diretorios
+ */
