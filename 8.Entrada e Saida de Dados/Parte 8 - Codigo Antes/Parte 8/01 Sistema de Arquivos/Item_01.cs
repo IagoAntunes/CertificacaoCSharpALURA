13633b9fee1d8969f1c5198b2d2278215ec49389
iagoa <iagoantunes.f@gmail.com>
2021-12-08 13:42:31 -0300

    Iago Antunes Ferreira

--- 8.Entrada e Saida de Dados/Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_01.cs
+++ 8.Entrada e Saida de Dados/Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_01.cs
@@ -13,7 +13,7 @@ namespace Listings
             FileStream fluxoSaida =
                 new FileStream("ArquivoSaida.txt",FileMode.Create,FileAccess.Write);
 
-            string mensagemSaida = "Ola, Alura bom dia";
+            string mensagemSaida = "Ola Alura, bom dia";
             
             byte[] array = Encoding.UTF8.GetBytes(mensagemSaida);
             int posicao = 0;
@@ -21,7 +21,27 @@ namespace Listings
             fluxoSaida.Write(array,posicao,tamanho);
             fluxoSaida.Close();
 
+            FileStream fluxoEntrada=
+                new FileStream("ArquivoSaida.txt",FileMode.Open,FileAccess.Read);
+
+            byte[] bytesLidos = new byte[fluxoEntrada.Length];
+
+            fluxoEntrada.Read(bytesLidos,posicao,(int)fluxoEntrada.Length);
+            string texto = Encoding.UTF8.GetString(bytesLidos);
+            fluxoEntrada.Close();
+            Console.WriteLine("Mensagem Lida: "+texto);
+            
+
             Console.ReadKey();
         }
     }
 }
+/*Abrir um fluxo de arquivo com Filetream
+ * FileMode.Open e FileAccess.Read
+ * Ler array de bytes com o metodo Read
+ * Pular posicoes com metodos seek e position
+ * Abrir um fluxo de escrita com file stream
+ * filemode.create e fileAccess.write
+ * Os metodos write e close
+ * Definir escopo do FileStream e liberand orecursos com using
+ */
