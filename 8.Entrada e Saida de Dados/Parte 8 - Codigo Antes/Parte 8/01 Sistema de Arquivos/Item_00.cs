56f38a278eab620adc454a6ed08c889863b23758
iagoa <iagoantunes.f@gmail.com>
2021-12-08 11:15:24 -0300

    Iago Antunes Ferreira

---
+++ 8.Entrada e Saida de Dados/Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_00.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Listings
+{
+    class Item_00 //Streams
+    {
+        static void Main(string[] args)
+        {
+            //TAREFAS:
+            //1. ABRIR O ARQUIVO Diretores.txt
+            //2. LER 10 BYTES DO ARQUIVO
+            //3. IMPRIMIR ESSES BYTES NO CONSOLE
+
+            FileStream fluxoDeArquivo =
+                new FileStream("Diretores.txt",FileMode.Open,FileAccess.Read);
+
+                
+                
+
+        }
+    }
+}
