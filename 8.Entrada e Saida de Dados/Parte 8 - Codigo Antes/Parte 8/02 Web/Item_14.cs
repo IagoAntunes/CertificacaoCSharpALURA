3edc9d3c96a8ce37b74e9cae66c023cc1ea72069
iagoa <iagoantunes.f@gmail.com>
2021-12-09 08:32:24 -0300

    Iago Antunes Ferreira

--- 8.Entrada e Saida de Dados/Parte 8 - Codigo Antes/Parte 8/02 Web/Item_14.cs
+++ 8.Entrada e Saida de Dados/Parte 8 - Codigo Antes/Parte 8/02 Web/Item_14.cs
@@ -7,7 +7,7 @@ namespace Listings
 {
     class Item_14 //WebClient
     {
-        static void Main(string[] args)
+        static void xMain(string[] args)
         {
             //TAREFA:
             //Conectar-se site da caelum (http://www.caelum.com.br)
