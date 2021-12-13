d3d7c915e9d26e3eba52ccab19b014c915df27cb
iagoa <iagoantunes.f@gmail.com>
2021-12-13 08:26:02 -0300

    Iago Antunes Ferreira

--- 10.Reflection/antes/Programa04/Program.cs
+++ 10.Reflection/antes/Programa04/Program.cs
@@ -51,6 +51,17 @@ namespace Programa04
             salario.Attributes = MemberAttributes.Public;
             funcionario.Members.Add(salario);
             //Tarefa 2.5: criar o construtor da classe
+            CodeConstructor construtor = new CodeConstructor();
+            construtor.Attributes = MemberAttributes.Public;
+            CodeParameterDeclarationExpression paramNome =
+                new CodeParameterDeclarationExpression(typeof(string), "nome");
+            construtor.Parameters.Add(paramNome);
+
+            CodeParameterDeclarationExpression paramSalario =
+                new CodeParameterDeclarationExpression(typeof(decimal), "salario");
+            construtor.Parameters.Add(paramSalario);
+
+            funcionario.Members.Add(construtor);
 
             codeNamespace.Types.Add(funcionario);
             unit.Namespaces.Add(codeNamespace);
