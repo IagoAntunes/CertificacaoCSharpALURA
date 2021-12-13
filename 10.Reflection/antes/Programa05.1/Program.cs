58a270dd8a3d8646cc265cc71f6be36e7269d56a
iagoa <iagoantunes.f@gmail.com>
2021-12-13 09:43:59 -0300

    Iago Antunes Ferreira

--- 10.Reflection/antes/Programa05.1/Program.cs
+++ 10.Reflection/antes/Programa05.1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Programa05_1.Modelo;
+using System;
 
 namespace Programa05
 {
@@ -7,10 +8,36 @@ namespace Programa05
         static void Main(string[] args)
         {
             //TAREFA 1: obter as propriedades de CarrinhoCliente
+
             //TAREFA 2: descobrir se podem ler ou escrever
             //TAREFA 3: descobrir seus acessadores getters e setters
 
+
+            var tipo = typeof(CarrinhoCliente);
+            var propriedades = tipo.GetProperties();
+            foreach (var propriedade in propriedades)
+            {
+                Console.WriteLine("Propriedade: {0}",propriedade.Name);
+
+                if (propriedade.CanRead)
+                {
+                    Console.WriteLine("\tPode ler");
+                    Console.WriteLine("\t\tMetodo Get: {0}",propriedade.GetMethod.Name);
+                }
+                if (propriedade.CanWrite)
+                {
+                    Console.WriteLine("\tPode escrever");
+                    Console.WriteLine("\t\tMetodo Set: {0}",propriedade.SetMethod.Name);
+                }
+            }
             Console.ReadLine();
         }
     }
 }
+/*05
+ * Usar tipos do namespace System.Reflection
+ * -Obtendo informações de assembly com a classe Assembly
+ * -Obtendo informações de propriedades com a classe PropertyInfo
+ * -Obtendo informações de metodos com a methodInfo
+ * -Obtendo informações de tipos com a classe type
+ */
