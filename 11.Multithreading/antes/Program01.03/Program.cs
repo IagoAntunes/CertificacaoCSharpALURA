321fbb14e0140770f692561a4c9fdc86f30bdffb
iagoa <iagoantunes.f@gmail.com>
2021-12-13 11:26:58 -0300

    Iago Antunes Ferreira

--- 11.Multithreading/antes/Program01.03/Program.cs
+++ 11.Multithreading/antes/Program01.03/Program.cs
@@ -16,6 +16,26 @@ namespace Program01._03
             //Tarefa 3: Interromper quando começar a processar o valor 75
             //Tarefa 4: Quantos itens foram processados (parcialmente)?
 
+            Console.WriteLine("Tarefa 1");
+            var resultadoLoop = Parallel.For(0,100,(int i,ParallelLoopState state) => 
+            {
+                if(i == 75)
+                {
+                    state.Break();
+                }
+                Processar(i); 
+            }
+            );
+
+            Console.WriteLine();
+
+            Console.WriteLine("Tarefa 2");
+            Console.WriteLine("Completou sem interrupcao ? {0}", resultadoLoop.IsCompleted) ;
+
+            Console.WriteLine("Tarefa 3");
+            Console.WriteLine("itens processados: {0}",resultadoLoop.LowestBreakIteration);
+
+
             Console.WriteLine("Término do processamento. Tecle [ENTER] para terminar.");
             Console.ReadLine();
         }
@@ -28,5 +48,14 @@ namespace Program01._03
         }
     }
 }
-
+/*01
+ * Task Parallel com Parallel.For,PLINQ,Tasks
+ * -Processamento em serie x processamento paralelo
+ * Executando actions em paralelo com o metodo Parallel invoke
+ * Executando faixa de action em paralelo com o metodo Parallel.For
+ * Exeuctando colecao de actions em paralelo com o metodo Parallel.Foreach
+ * Usando um cronometro StopWatch para calcular o tempo decorrido
+ * Detectar se uma tarefa completou sem interrupção
+ * Calculando quantos itens foram processados parcialmente
+ */
 
