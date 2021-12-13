d1eef18a6ca410a42af7183335ad85581b10cfcc
iagoa <iagoantunes.f@gmail.com>
2021-12-13 16:50:13 -0300

    Iago Antunes Ferreira

--- 11.Multithreading/antes/Program01.08/Program.cs
+++ 11.Multithreading/antes/Program01.08/Program.cs
@@ -10,6 +10,44 @@ namespace Program01._10
         {
             //PROBLEMA: Criar e executar uma tarefa-mãe 
             //e 10 tarefas-filhas que levam 1s cada uma para terminar.
+
+            Task tarefaMae = 
+                Task.Factory.StartNew(() =>{
+                    Console.WriteLine("Tarefa-Mae iniciou");
+
+                    for(int i = 0; i < 10; i++)
+                    {
+                        int tarefaId = i;
+
+                        Task tarefaFilha =
+                        Task.Factory.StartNew((id) => 
+                        ExecutarFilha(id),
+                        tarefaId,
+                        TaskCreationOptions.AttachedToParent);
+                    }
+
+                });
+            tarefaMae.Wait();
+            Console.WriteLine("Tarefa-Mae terminou");
+
+
+            Console.ReadLine();
+        }
+
+        private static void ExecutarFilha(object i)
+        {
+            Console.WriteLine("\tTarefa-filha {0} Iniciou",i);
+            Thread.Sleep(1000);
+            Console.WriteLine("\tTarefa-filha {0} Terminou",i);
         }
     }
 }
+/*03
+ * Espera, continuacao e hierarquia de tarefas
+ * Enfileirando tarefa no ThreadPool com o metodo Task.Run()
+ * Aguardando as tarefas serem completadas com o metodo Task.WaitAll()
+ * Contando o numero de threads atual com Process.GetCurrentProcess()Threads.Count
+ * Criando tarefas de continuação de tarefas com Task.ContinueWith()
+ * Criando e iniciando com Task.Factory.StartNew()
+ * Associando tarefa-mae e tarefas-filhas com a opção TaskCreationOptions.AttachedToParent
+ */
