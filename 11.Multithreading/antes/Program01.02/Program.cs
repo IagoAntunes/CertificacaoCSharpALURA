43bfcfad36f5a0a966a213dde735281107e110d4
iagoa <iagoantunes.f@gmail.com>
2021-12-13 10:56:57 -0300

    Iago Antunes Ferreira

--- 11.Multithreading/antes/Program01.02/Program.cs
+++ 11.Multithreading/antes/Program01.02/Program.cs
@@ -15,17 +15,36 @@ namespace Program01._02
             //Tarefa 1: processar 100 itens em série
             //Tarefa 2: processar 100 itens em paralelo - percorrendo uma faixa
             //Tarefa 3: processar 100 itens em paralelo - percorrendo uma coleção
+            Stopwatch stopwatch = new Stopwatch();
 
-            Console.WriteLine("Tarefa");
+            stopwatch.Start();
+            Console.WriteLine("Tarefa 1");
             for(int i = 0; i < 100; i++)
             {
                 Processar(i);
             }
+            stopwatch.Stop();
+            Console.WriteLine("Tempo 1 : {0}", stopwatch.ElapsedMilliseconds / 1000.0);
+            stopwatch.Reset();
+
             Console.WriteLine();
 
+            stopwatch.Restart();
             Console.WriteLine("Tarefa 2");
             Parallel.For(0, 100, i => Processar(i));
+            stopwatch.Stop();
+            Console.WriteLine("Tempo 2: {0}",stopwatch.ElapsedMilliseconds / 1000.0);
+            stopwatch.Reset();
 
+            Console.WriteLine();
+
+            stopwatch.Start();
+            Console.WriteLine("Tarefa 3");
+            var itens = Enumerable.Range(0, 100);
+            Parallel.ForEach(itens, (item) => Processar(item));
+            stopwatch.Stop();
+            Console.WriteLine("Tempo 3: {0}", stopwatch.ElapsedMilliseconds / 1000.0);
+            Console.WriteLine();
 
             Console.WriteLine("Término do processamento. Tecle [ENTER] para terminar.");
             Console.ReadLine();
