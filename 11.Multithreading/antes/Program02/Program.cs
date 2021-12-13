99c9e9e0943a65380663a7627a53f2b915839505
iagoa <iagoantunes.f@gmail.com>
2021-12-13 17:20:05 -0300

    Iago Antunes Ferreira

--- 11.Multithreading/antes/Program02/Program.cs
+++ 11.Multithreading/antes/Program02/Program.cs
@@ -8,11 +8,17 @@ namespace Program02
         static void Main(string[] args)
         {
             //1. Task X Thread
-
+            Thread thread1 = new Thread(Executar);
+            thread1.Start();
             //2. Thread com expressão lambda
-
+            Thread thread2 = new Thread(()=>Executar());
+            thread2.Start();
             //3. Passando parâmetro para thread
+            ParameterizedThreadStart ps =
+                new ParameterizedThreadStart((p)=>ExecutarComParametro(p));
 
+            Thread thread3 = new Thread(ps);
+            thread3.Start();
             //4. Interrompendo um relógio
 
             //5. Sincronizando uma thread
@@ -20,7 +26,19 @@ namespace Program02
             //6. Dados da Thread: Nome, cultura, prioridade, contexto, background, pool
 
             //7. Usando Thread Pool
-           
+            Console.ReadLine();
+        }
+        static void Executar()
+        {
+            Console.WriteLine("Inicio da execucao");
+            Thread.Sleep(1000);
+            Console.WriteLine("Fim da execucao");
+        }        
+        static void ExecutarComParametro(object param)
+        {
+            Console.WriteLine("Inicio da execucao: {0}",param);
+            Thread.Sleep(1000);
+            Console.WriteLine("Fim da execucao: {0}",param);
         }
     }
 }
